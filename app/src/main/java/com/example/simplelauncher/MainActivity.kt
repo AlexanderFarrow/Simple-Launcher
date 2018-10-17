e143ed0e084a417115e9f0740b43dbf542c34248
Alexander <AlexanderFarrow98@gmail.com>
2018-10-17 22:47:14 +0100

    Get a list of installed apps.

--- app/src/main/java/com/example/simplelauncher/MainActivity.kt
+++ app/src/main/java/com/example/simplelauncher/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.simplelauncher
 
+import android.content.Context
+import android.content.pm.LauncherActivityInfo
+import android.content.pm.LauncherApps
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.UserManager
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getInstalledApps()
     }
+
+    private fun getInstalledApps() {
+        val userManager = getSystemService(Context.USER_SERVICE) as UserManager
+        val launcher = getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
+
+        val apps: List<LauncherActivityInfo> = launcher.getActivityList(null, userManager.userProfiles[0])
+
+        for (app in apps) {
+            val appName = app.label.toString()
+            Log.i("Alex", appName)
+        }
+
+    }
+
 }
